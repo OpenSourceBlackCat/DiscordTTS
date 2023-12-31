35aca537a5977d452e045a2d0ffdcb6c9d4705dd
OpenSourceBlackCat <amey992004@gmail.com>
2023-12-31 15:54:30 +0530

    Added Link Filter & Using Server Nicknames

--- TTS/TTS.py
+++ TTS/TTS.py
@@ -3,16 +3,17 @@ from discord import Intents, FFmpegPCMAudio
 from emoji import distinct_emoji_list, demojize
 from googletrans import Translator
 from dotenv import load_dotenv
+from re import sub, findall
 from gtts import gTTS
 from os import getenv
-from re import sub
 load_dotenv()
 AmeyaBot = Bot(intents=Intents.all())
 _GUILD=getenv("GUILD")
 _OWNER=getenv("OWNER")
 _LAST_SPOKEN_USER = ""
 _AUDIO_FILE = "meow.mp3"
-_REGEX = r"<(\w+)?:(\w+):(\d+)>"
+_REGEX_EMOJI = r"<(\w+)?:(\w+):(\d+)>"
+_REGEX_LINK = r""
 _TRANSLATOR = Translator()
 class TTS:
     async def talk(text):
@@ -51,12 +52,14 @@ class TTS:
             if(Auth_User.voice):
                 if(Auth_User.voice.channel.id==ctx.channel.id):
                     for mention in ctx.mentions:
-                        ctx.content = ctx.content.replace(f"<@{mention.id}>", mention.name)
-                    ctx.content = sub(_REGEX, "Emoji", ctx.content)
+                        ctx.content = ctx.content.replace(f"<@{mention.id}>", mention.nick)
+                    if(findall(_REGEX_LINK, ctx.content)):
+                        ctx.content = sub(_REGEX_LINK, "Link", ctx.content)
+                    ctx.content = sub(_REGEX_EMOJI, "Emoji", ctx.content)
                     for emoji in distinct_emoji_list(ctx.content):
                         ctx.content = ctx.content.replace(emoji, _TRANSLATOR.translate(text=str(demojize(emoji)).replace("_", " "), dest="hi").text)
                     if(_LAST_SPOKEN_USER==ctx.author.name):
                         await TTS.talk(ctx.content)
                     else:
-                        await TTS.talk(f"{ctx.author.name} Says {ctx.content}")
+                        await TTS.talk(f"{ctx.author.nick} Says {ctx.content}")
                     _LAST_SPOKEN_USER = ctx.author.name
