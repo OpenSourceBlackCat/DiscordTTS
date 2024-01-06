16b1c09d707d6402ebd2c0df6c2f33e8d0a16f29
OpenSourceBlackCat <amey992004@gmail.com>
2024-01-06 06:12:10 +0530

    [WIP] Added Funny Sounds Support.

--- TTS/TTS.py
+++ TTS/TTS.py
@@ -1,6 +1,7 @@
 from discord.ext.commands import Bot
 from discord import Intents, FFmpegPCMAudio
 from emoji import distinct_emoji_list, demojize
+from urllib.request import urlretrieve
 from googletrans import Translator
 from dotenv import load_dotenv
 from re import sub, findall
@@ -10,22 +11,28 @@ load_dotenv()
 AmeyaBot = Bot(intents=Intents.all())
 _GUILD=getenv("GUILD").split(",")
 _AUTH_PERSON=getenv("AUTH_PERSON").split(",")
+_SOUND_LIB = "https://gitlab.com/OpenSourceBlackCat/MeowBotAssets/-/raw/main/audio/"
 _LAST_SPOKEN_USER = ""
 _AUDIO_FILE = "meow.mp3"
 _REGEX_EMOJI = r"<(\w+)?:(\w+):(\d+)>"
 _REGEX_LINK = r"(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])"
 _TRANSLATOR = Translator()
 class TTS:
-    async def talk(text):
-        textClient = gTTS(text=text, lang="hi")
-        textClient.save("meow.mp3")
+    async def talk(text=None, URL=None):
+        AUDIO_FILE = ""
+        if(text):
+            textClient = gTTS(text=text, lang="hi")
+            AUDIO_FILE = "meow.mp3"
+            textClient.save(AUDIO_FILE)
+        elif(URL):
+            AUDIO_FILE = URL
         for voiceClient in AmeyaBot.voice_clients:
             if voiceClient.is_playing():
                 try:
-                    voiceClient.play(FFmpegPCMAudio(_AUDIO_FILE))
+                    voiceClient.play(FFmpegPCMAudio(AUDIO_FILE))
                 except: pass
             else:
-                voiceClient.play(FFmpegPCMAudio(_AUDIO_FILE))
+                voiceClient.play(FFmpegPCMAudio(AUDIO_FILE))
     @AmeyaBot.slash_command(guild_ids=_GUILD, description="Connect To The Channel.")
     async def meow(ctx):
         if(str(ctx.author.id) in _AUTH_PERSON):
@@ -49,6 +56,16 @@ class TTS:
                     await ctx.respond("The Authorised Admin Is Not Connected To The Channel.")
         else:
             await ctx.respond("You're Not Authorised To Operate This Bot!")
+    @AmeyaBot.slash_command(guild_ids=_GUILD, descriptiop="To Play Some Funny Sounds.")
+    async def sounds(ctx, sound):
+        if(str(ctx.author.id) in _AUTH_PERSON):
+            if(ctx.author.voice):
+                if(ctx.author.voice.channel.id==ctx.channel.id):
+                    try:
+                        await TTS.talk(URL=f"{_SOUND_LIB}/{sound}.mp3")
+
+                    except Exception as e:
+                        print("File Not Found", e)
     @AmeyaBot.event
     async def on_ready():
         print("BlackCat's TTS BOT Running.")
@@ -65,10 +82,10 @@ class TTS:
                 for emoji in distinct_emoji_list(ctx.content):
                     ctx.content = ctx.content.replace(emoji, _TRANSLATOR.translate(text=str(demojize(emoji)).replace("_", " "), dest="hi").text)
                 if(_LAST_SPOKEN_USER==ctx.author.name):
-                    await TTS.talk(ctx.content)
+                    await TTS.talk(text=ctx.content)
                 else:
                     if(ctx.author.nick):
-                        await TTS.talk(f"{ctx.author.nick} Says {ctx.content}")
+                        await TTS.talk(text=f"{ctx.author.nick} Says {ctx.content}")
                     else:
-                        await TTS.talk(f"{ctx.author.name} Says {ctx.content}")
+                        await TTS.talk(text=f"{ctx.author.name} Says {ctx.content}")
                 _LAST_SPOKEN_USER = ctx.author.name
